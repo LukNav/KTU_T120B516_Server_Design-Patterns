f176c4f6320d38fc3222a83e4ca0a5666ae3bf6a
Lukas Navašinskas <Lukas.Navasinskas@adform.com>
2022-10-03 16:17:31 +0300

    Added new Game controller

---
+++ Server/Controllers/GameController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.GameLogic;
+
+namespace Server.Controllers
+{
+    public class GameController : Controller
+    {
+        private GameSession _gameSession = new GameSession();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
