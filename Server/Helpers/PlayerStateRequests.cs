ce158a64f44e6001b620b744c420e7fee24a538c
RUTA <r.bartkute@ktu.edu>
2022-11-07 01:35:40 +0200

    Added player state requests

---
+++ Server/Helpers/PlayerStateRequests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Net.Http.Headers;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Server.Helpers
+{
+    public class PlayerStateRequests
+    {
+        Player _player { get; set; }
+
+        public PlayerStateRequests(Player player)
+        {
+            _player = player;
+        }
+
+        public GameState GetState()
+        {
+            string getEndpoint = "/GetGameState/";
+            HttpResponseMessage httpResponseMessagePlayerOne = HttpRequests.GetRequest(_player.IpAddress + getEndpoint);
+            return httpResponseMessagePlayerOne.Deserialize<GameState>();
+        }
+    }
+}
