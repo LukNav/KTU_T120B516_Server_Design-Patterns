605d3e53a80000ae8bc3c37054bbe68d2e289b8b
RUTA <r.bartkute@ktu.edu>
2022-11-07 21:19:27 +0200

    Created Facade Interface

---
+++ Server/GameLogic/IClientSessionFacade.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.GameLogic
+{
+    public interface IClientSessionFacade
+    {
+        ActionResult<string> AddClient(string name, string ip);
+        ActionResult RemoveClient(string name);
+        Task<IActionResult> SetClientReady(string name);
+    }
+}
