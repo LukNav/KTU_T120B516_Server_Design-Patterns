e748f736f4f3028bee28f0a374896ec7a2e4a47f
RUTA <r.bartkute@ktu.edu>
2022-11-07 02:30:11 +0200

    Split files, fixed bug

--- Server/Helpers/PlayerStateRequests.cs
+++ Server/Helpers/PlayerStateRequests.cs
@@ -27,24 +27,4 @@ namespace Server.Helpers
             return HttpRequests.GetRequest(_player.IpAddress + getEndpoint);
         }
     }
-
-    interface IPlayerStateRequestsAdapter
-    {
-        GameState GetState();
-    }
-
-    public class PlayerStateRequestsAdapter : IPlayerStateRequestsAdapter
-    {
-        PlayerStateRequests _playerStateRequests;
-
-        public PlayerStateRequestsAdapter(Player player)
-        {
-            _playerStateRequests = new PlayerStateRequests(player);
-        }
-
-        public GameState GetState()
-        {
-            return _playerStateRequests.GetState().Deserialize<GameState>(;
-        }
-    }
 }
