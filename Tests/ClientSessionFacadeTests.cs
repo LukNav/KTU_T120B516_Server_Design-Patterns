cdced7d2ac82b916b94c8e20cebb418daa21b4cd
RUTA <r.bartkute@ktu.edu>
2022-12-04 18:06:21 +0200

    Added Unit Project AddClient unit test

---
+++ Tests/ClientSessionFacadeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Server.GameLogic;
+
+namespace Tests 
+{
+    internal class ClientSessionFacadeTests : ControllerBase
+    {
+        ClientSessionFacade clientSessionFacade;
+
+        [SetUp]
+        public void Setup()
+        {
+            clientSessionFacade = new ClientSessionFacade();    
+        }
+
+        [Test]
+        public void AddClient_ValidInput_ReturnsCreatedValue ()
+        {
+            const string name = "testName";
+            ActionResult<string> result = clientSessionFacade.AddClient(name, "kpafdkn");
+            ActionResult<string> expected = Created("", $"Player '{name}' was created");
+            Assert.That(result.Value, Is.EqualTo(expected.Value));
+        }
+    }
+}
