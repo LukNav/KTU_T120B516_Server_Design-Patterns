32c9fa44c917616213c978fa78c8a12272f177d8
VincentasG <vincas.genys@gmail.com>
2022-12-20 02:51:05 +0200

    Simple state implementation

---
+++ Server/GameLogic/StatePattern/State.cs
@@ -0,0 +1,24 @@
+﻿using Server.Models;
+
+namespace Server.GameLogic.StatePattern
+{
+    public abstract class State
+    {
+        private Pawn pawn;
+        public bool IsSelectable { get; set; }
+
+        public Pawn Pawn
+        {
+            get
+            {
+                return pawn;
+            }
+            set
+            {
+                pawn = value;
+            }
+        }
+
+        public abstract void SwitchState();
+    }
+}
